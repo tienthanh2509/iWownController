94aae554ba49658a78883c3dd37b2e329e4112fb
Phạm Tiến Thành <tienthanh.dqc@gmail.com>
2017-05-14 12:38:50 +0700

    App > iwown
    Signed-off-by: Phạm Tiến Thành <tienthanh.dqc@gmail.com>

---
+++ iwown/src/main/java/ru/wilix/device/geekbracelet/App.java
@@ -0,0 +1,65 @@
+package ru.wilix.device.geekbracelet;
+
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
+
+/**
+ * Created by Aloyan Dmitry on 29.08.2015
+ */
+public class App extends Application {
+    public static Context mContext;
+    public static SharedPreferences sPref;
+
+    public static void loadProperties() {
+        SharedPreferences sp = App.sPref;
+        if (sp.getBoolean("fit_connected", false))
+            GoogleFitConnector.connect(App.mContext);
+    }
+
+    @Override
+    public void onCreate() {
+        ACRA.init(this);
+        super.onCreate();
+        App.mContext = getApplicationContext();
+        App.sPref = PreferenceManager.getDefaultSharedPreferences(App.mContext);
+
+        if (BLEService.isBluetoothAvailable()) {
+            // Create service
+            Intent gattServiceIntent = new Intent(this, BLEService.class);
+            startService(gattServiceIntent);
+
+            PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 0,
+                    new Intent(this, MainActivity.class), 0);
+
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher))
+                    .setSmallIcon(R.drawable.ic_launcher)
+                    .setContentText("WiliX Controller")
+                    .setContentTitle("WiliX Controller")
+                    .setTicker("WiliX Controller")
+                    .setPriority(Notification.PRIORITY_LOW)
+                    .setAutoCancel(false)
+                    .setContentIntent(pi)
+                    .build();
+
+            notification.flags |= Notification.FLAG_FOREGROUND_SERVICE | Notification.FLAG_INSISTENT;// Notification.FLAG_NO_CLEAR | Notification.FLAG_ONGOING_EVENT |
+            ((NotificationManager) getSystemService(NOTIFICATION_SERVICE)).notify(0, notification);
+            loadProperties();
+        } else {
+            Toast.makeText(App.mContext, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
+    }
+}
---
+++ iwown/src/main/java/ru/wilix/device/geekbracelet/receiver/NotificationMonitor.java
@@ -0,0 +1,153 @@
+package ru.wilix.device.geekbracelet.receiver;
+
+import android.app.Notification;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
+import java.io.Serializable;
+
+import ru.wilix.device.geekbracelet.BroadcastConstants;
+import ru.wilix.device.geekbracelet.i5.Constants;
+import ru.wilix.device.geekbracelet.model.AppNotification;
+
+/**
+ * Created by Aloyan Dmitry on 30.08.2015
+ */
+public class NotificationMonitor extends NotificationListenerService {
+    public static StatusBarNotification lastSbn;
+
+    public static boolean settingsKeepForeign = false;
+    public static int settingsDelay = 0;
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        Log.i("NOTIFICATION", "From package: " + sbn.getPackageName());
+        lastSbn = sbn;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if (settingsDelay != 0) {
+                        StatusBarNotification notif = lastSbn.clone();
+                        Thread.sleep(settingsDelay * 1000);
+                        if (lastSbn == null || lastSbn.getPostTime() != notif.getPostTime()) return;
+                    }
+                    int canNotice = AppNotification.canNotice(lastSbn.getPackageName());
+                    if (canNotice > 0 && lastSbn.isClearable()) {
+                        Intent intent = new Intent(BroadcastConstants.ACTION_NEW_NOTIFICATION_RECEIVED);
+                        intent.putExtra("data", Notif.fromSbn(lastSbn, canNotice));
+                        sendBroadcast(intent);
+                        return;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (lastSbn != null)
+                    Log.i("NOTIFICATION", "Package: " + lastSbn.getPackageName() + " skipped");
+            }
+        }).start();
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+        // Log.i("NOTIF!", "On remove");
+        if (settingsDelay != 0) {
+            if (lastSbn != null && sbn.getPackageName().equals(lastSbn.getPackageName())) {
+                lastSbn = null;
+            }
+        }
+    }
+
+    public IBinder onBind(Intent intent) {
+        return super.onBind(intent);
+    }
+
+    public static class Notif implements Serializable {
+        public String appName = "";
+        public String fromName = "";
+        public String msgText = "";
+        public Integer noticeType = 0;
+
+        public static Notif fromSbn(StatusBarNotification sbn, int notice_type) {
+            Notif nf = new Notif();
+            //String ticker = "";
+
+            if (sbn == null || sbn.getPackageName() == null)
+                return nf;
+
+            Log.i("Package", sbn.getPackageName());
+
+            if (sbn.getNotification() == null)
+                return nf;
+
+//            if( sbn.getNotification().tickerText != null ) {
+//                ticker = sbn.getNotification().tickerText.toString();
+//            }
+            if (sbn.getNotification().extras != null) {
+                Bundle extras = sbn.getNotification().extras;
+                if (extras.containsKey(Notification.EXTRA_TITLE))
+                    nf.fromName = extras.get(Notification.EXTRA_TITLE).toString();
+                if (extras.containsKey("android.text"))
+                    nf.msgText = extras.get("android.text").toString();
+            }
+
+//            Log.i("Ticker", ticker);
+            if (nf.fromName != null)
+                Log.i("Title", nf.fromName);
+//            if( nf.msgText != null )
+//                Log.i("Text", nf.msgText);
+            nf.noticeType = notice_type;
+
+            return nf;
+        }
+
+        public Integer getDeviceNoticeType() {
+            switch (this.noticeType) {
+                case 1:
+                    return Constants.ALERT_TYPE_MESSAGE;
+                case 2:
+                    return Constants.ALERT_TYPE_CLOUD;
+                case 3:
+                    return Constants.ALERT_TYPE_ERROR;
+                default:
+                    return Constants.ALERT_TYPE_MESSAGE;
+            }
+        }
+
+        public String getAppName() {
+            return this.appName;
+        }
+
+        public void setAppName(String value) {
+            this.appName = value;
+        }
+
+        public String getFromName() {
+            return this.fromName;
+        }
+
+        public void setFromName(String value) {
+            this.fromName = value;
+        }
+
+        public String getMsgText() {
+            return this.msgText;
+        }
+
+        public void setMsgText(String value) {
+            this.msgText = value;
+        }
+
+        public Integer getNoticeType() {
+            return this.noticeType;
+        }
+
+        public void setNoticeType(Integer type) {
+            this.noticeType = type;
+        }
+    }
+}
